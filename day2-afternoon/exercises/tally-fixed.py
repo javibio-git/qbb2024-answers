246a34adc0bf6f1854f3c079fce2b1d006648e48
Javier Carpinteyro-Ponce <jcarpinteyro@carnegiescience.edu>
2024-09-04 17:42:13 -0400

    finally figured out the tally exercise

--- day2-afternoon/exercises/tally-fixed.py
+++ day2-afternoon/exercises/tally-fixed.py
@@ -7,18 +7,19 @@ import sys
 
 my_file = open( sys.argv[1] )
 
-chr = ""
+chr = "chr1"
 count = 0
 
 for my_line in my_file:
     if "#" in my_line:
         continue
     fields = my_line.split("\t")
-    if fields[0] !q= chr:
+    if fields[0] != chr:
         print( count, chr )
         chr = fields[0]
-        count = 0
+        count = 1
         continue
     count = count + 1
+print( count, chr)
 
 my_file.close()
